f2bafb651f75fb39419dc5a037d2e184b7c88f56
Abdulhakim <Abdulthe383@gmail.com>
2025-01-24 23:46:32 -0500

    task 2

--- cc_1.js
+++ cc_1.js
@@ -9,3 +9,11 @@ console.log( typeof employeeName);
 console.log (typeof employeeID);
 console.log( typeof isActive);
 
+// Task 2 primitive data types //
+let ProductName = "ichigo hallow mask"; //product name
+const ProductPrice = 15.00; // product price
+var isAvailable = true;
+
+console.log(typeof ProductName);
+console.log (typeof ProductPrice);
+console.log (typeof isAvailable);

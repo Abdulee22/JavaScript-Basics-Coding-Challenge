4e3ad32feeca5fd804207ebd7ddd376630f8fa13
Abdulhakim <Abdulthe383@gmail.com>
2025-01-24 23:44:41 -0500

    task 1

---
+++ cc_1.js
@@ -0,0 +1,11 @@
+
+// Task 1 employee info //
+
+let employeeName = "Ichigo Kurasaki"; // the employee name
+const employeeID = 10000;  // the employee id
+var isActive = true;   // employee status in the company 
+
+console.log( typeof employeeName);
+console.log (typeof employeeID);
+console.log( typeof isActive);
+
